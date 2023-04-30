5eaa0a7d3ea866a9a6992748635e6b871d7f56bc
Michrira <Michaelchristianrader@gmail.com>
2023-04-29 17:26:57 -0700

    edited routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./homeroutes.js');
+const dashboardRoutes = require('./dashboardroutes.js');
 
 
 router.use('/api', apiRoutes);
